8a5e6707cb47fac7f675d252e2de25e3beb10dcb
Jesse Wolgamott <jesse@comalproductions.com>
2013-04-12 14:38:13 -0500

    Inherit storage type from fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,12 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   include Sprockets::Rails::Helper
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url
